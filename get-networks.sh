a64494d75387ea5a355cc67b26b8defde1e9b22c
sergiomsilva <sergio.ms2@gmail.com>
2018-08-19 11:16:58 -0300

    Update get-networks.sh

--- get-networks.sh
+++ get-networks.sh
@@ -6,15 +6,15 @@ mkdir data/lp-detector -p
 mkdir data/ocr -p
 mkdir data/vehicle-detector -p
 
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/lp-detector/wpod-net.h5   -P data/lp-detector/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/lp-detector/wpod-net.json -P data/lp-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/lp-detector/wpod-net.h5   -P data/lp-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/lp-detector/wpod-net.json -P data/lp-detector/
 
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/ocr/ocr-net.cfg     -P data/ocr/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/ocr/ocr-net.names   -P data/ocr/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/ocr/ocr-net.weights -P data/ocr/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/ocr/ocr-net.data    -P data/ocr/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/ocr/ocr-net.cfg     -P data/ocr/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/ocr/ocr-net.names   -P data/ocr/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/ocr/ocr-net.weights -P data/ocr/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/ocr/ocr-net.data    -P data/ocr/
 
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/vehicle-detector/yolo-voc.cfg     -P data/vehicle-detector/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/vehicle-detector/voc.data         -P data/vehicle-detector/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/vehicle-detector/yolo-voc.weights -P data/vehicle-detector/
-wget -c -N www.inf.ufrgs.br/~smsilva/eccv_2018/data/vehicle-detector/voc.names        -P data/vehicle-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/vehicle-detector/yolo-voc.cfg     -P data/vehicle-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/vehicle-detector/voc.data         -P data/vehicle-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/vehicle-detector/yolo-voc.weights -P data/vehicle-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/vehicle-detector/voc.names        -P data/vehicle-detector/
