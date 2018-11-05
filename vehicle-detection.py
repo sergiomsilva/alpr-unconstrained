a4fda9111ea6d446f6f606958802f7218d242a6c
Sérgio Montazzolli <sergio.ms2@gmail.com>
2018-11-05 16:07:18 -0200

    YOLO output filtering inserted.

--- vehicle-detection.py
+++ vehicle-detection.py
@@ -41,6 +41,8 @@ if __name__ == '__main__':
 
 		R = detect(vehicle_net, vehicle_meta, img_path ,thresh=vehicle_threshold)
 
+		R = [r for r in R if r[0] in ['car','bus']]
+
 		print '\t\t%d cars found' % len(R)
 
 		if len(R):
