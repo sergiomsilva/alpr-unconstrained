eec011524adde34aed8fad6e6ae1423ab272403e
SothanaV <nai6320@gmail.com>
2019-11-25 18:32:49 +0700

    add coco.data&coco.names

--- prepairPython.sh
+++ prepairPython.sh
@@ -2,3 +2,5 @@ pip3 install --upgrade tensorflow-gpu
 pip3 install numpy scipy scikit-learn pillow h5py
 pip3 install keras
 mv ./yolov2.cfg ./data/vehicle-detector
+mv ./coco.data ./data/vehicle-detector
+mv ./coco.names ./data/vehicle-detector
